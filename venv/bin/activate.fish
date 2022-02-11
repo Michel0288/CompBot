0f494aabfec209f01e681b8df531af4e67f0068a
PatreceCampbell <patrece.campbell@mymona.uwi.edu>
2022-02-11 02:00:38 -0500

    Started Hompage Design

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/michel/CompBot/venv"
+set -gx VIRTUAL_ENV "/home/patrece/Desktop/CompBot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
