c7a5a7dd8f7b8bb1ea1d4d9e91b7c753e2ea6e12
MiDevenKnow <kabiandavison19@gmail.com>
2022-02-28 21:58:19 -0500

    New Homagepage Design

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,8 @@
 /* Add your Application JavaScript */
+$(window).resize(function() {
+    if ($(window).width() <= 991) {
+        $("#navbarNavAltMarkup").addClass("border-bottom");
+    }else{
+        $("#navbarNavAltMarkup").removeClass("border-bottom");
+    }
+});
