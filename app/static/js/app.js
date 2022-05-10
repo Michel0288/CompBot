02d7cd781c0be10c1d9f8ed8ac9483d51cbcd603
MiDevenKnow <kabiandavison19@gmail.com>
2022-05-10 07:12:32 -0500

    New landing page

--- app/static/js/app.js
+++
@@ -1,8 +0,0 @@
-/* Add your Application JavaScript */
-$(window).resize(function() {
-    if ($(window).width() <= 991) {
-        $("#navbarNavAltMarkup").addClass("border-bottom");
-    }else{
-        $("#navbarNavAltMarkup").removeClass("border-bottom");
-    }
-});
